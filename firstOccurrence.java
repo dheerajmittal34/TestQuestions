a2eb40825b9cae5be8141cb10bbd3ca57208c614
Dheeraj Mittal <dheerajmittal098@gmail.com>
2025-10-07 02:58:29 +0530

    Fantastic

---
+++ firstOccurrence.java
@@ -0,0 +1,27 @@
+public class firstOccurrence {
+    public static int firstOccurrence(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) {
+                result = mid;        
+                left = mid + 1;      
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid + 1;
+            }
+        }
+
+        return result;  
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 3, 4, 5, 6, 7, 8};
+        int key = 2;
+        System.out.println("First occurrence of " + key + ": " + firstOccurrence(arr, key)); 
+    }
+}
